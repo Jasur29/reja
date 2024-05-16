7419e357525d9c0323d55e451656fe1d68b8828f
Jasur29 <kaharov2017gmail.com>
2024-05-16 16:58:33 +0900

    BRR: building express web server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//expressJS. Nodejs platformasida oson va qulay kod yozish imkonini beradigan web framework.
+
 console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
@@ -7,14 +9,19 @@ const http = require("http");
 
 //1 bosqich-expressga kirib kelayotgan malumotlarga bogliq kodlar yoziladi
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // htmldan form request qilinganda express serverimiz qabul qilishi uchun
+app.use(express.static("public")); //browserdan kirib kelayotgan har qandar zaproslar uchun "public" folderi ochiq.
+app.use(express.json()); // kirib kelayotgan json holatidagi datani object holatiga ogirib beradi.
+//client va server ortasida malumotlar json formatda boladi
+app.use(express.urlencoded({ extended: true })); // htmldan traditional form request qilinganda express serverimiz qabul qilishi uchun. agar
+//bu kod yozilmasa serverimiz htmldan kiritildan malumotlarni ignore qiladi.
 
-//2 sessionga bogliq kodlar yoziladi
+//2 sessionga bogliq kodlar yoziladi/////////////////
 
-//3 bosqichda ( express uchun BSSR  back end server site randoring  tanlandi ) yani back endda front endi quramiz yani view quramiz bu traditional yol xisoblanadi
-// back endda html yasab uni clientga yuboramiz. aynan shuning uchun view yasash uchun folder kerak bunda ejsdan foydalanamiz. va uni instal qivolamiz (npm i ejs)
+//3 bosqichda ( express uchun BSSR  "backend server site randoring"  tanlandi ) yani backendda front endi quramiz yani view quramiz
+//yani html
+// bu traditional yol xisoblanadi
+// back endda html yasab uni clientga yuboramiz. aynan shuning uchun view yasash uchun folder kerak bunda ejsdan foydalanamiz.
+// va uni instal qivolamiz (npm i ejs)
 // va undan song kod kiritamiz
 
 // views ga bogliq kodlar
@@ -23,7 +30,7 @@ app.set("view engine", "ejs"); // bunda view engine ejs ekanligini korsatilib be
 
 // yani ejs orqali backend ichida front end yasaymiz.
 
-//4 bosqich bu  rooterlarga moljallangan bogliq kodlar.
+//4 bosqich bu  routingga moljallangan bogliq kodlar.
 
 // app.get("/hello", function (req, res) {
 //   res.end(`<h1 style="background:red">HELLO WORLD by LEO</h1>`);
@@ -39,7 +46,7 @@ app.get("/", function (req, res) {
   res.render("harid");
 });
 
-const server = http.createServer(app); // server hosil qilindi
+// const server = http.createServer(app); // server hosil qilindi
 let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The sever is running successfully on port: ${PORT}`); // muvafaqqiyatli ishlasa bu chiqadi.
